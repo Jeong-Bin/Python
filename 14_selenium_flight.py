b1de2cbc08decacc6a186da7056093f82e808a46
홍정빈 <hjb3880@gmail.com>
2021-01-19 12:12:48 +0900

    네이버 항공편 스크래핑(중간로딩 처리)

--- 14_selenium_flight.py
+++ 14_selenium_flight.py
@@ -28,6 +28,23 @@ browser.find_element_by_xpath("//*[@id='recommendationList']/ul/li[1]").click()
 # "항공권 검색 클릭"
 browser.find_element_by_link_text("항공권 검색").click()
 
-# 첫 번째 결과 출력
-elem = browser.find_element_by_xpath("//*[@id='content']/div[2]/div/div[4]/ul/li[1]")
-print(elem.text)
+# 첫 번째 결과 출력.  이 방법은 로딩 때문에 안 됨
+# elem = browser.find_element_by_xpath("//*[@id='content']/div[2]/div/div[4]/ul/li[1]")
+# print(elem.text)
+
+
+# 중간 로딩시간 때문에 로딩 기다린 후 로딩이 끝나면 이후 코드가 작동하도록 해야함
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# 브라우저를 최대 10초를 기다리되, 다음 엘리먼트가 나오면 바로 진행
+# 만약 10초가 지나도 로딩이 안 끝나면 에러가 나기때문에 그냥 끝내도록 함. -> try:, finally: 사용 
+try:
+    elem = WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, "//*[@id='content']/div[2]/div/div[4]/ul/li[1]")))
+                                                                            # XPATH 외에도 ID, CLASS_NAME, LINK_TEXT 등도 사용 가능함
+    print(elem.text)  # 첫 번째 항공편 결과를 터미널로 출력
+
+finally:
+    browser.quit()
+
